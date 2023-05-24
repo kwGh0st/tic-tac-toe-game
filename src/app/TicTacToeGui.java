352476446f949c4ed02bf081eecaf70e524df15b
Karol Programuje <karol341654@gmail.com>
2023-05-24 20:16:17 +0200

    Adding game table (buttons). Adding them to the panel.

--- src/app/TicTacToeGui.java
+++ src/app/TicTacToeGui.java
@@ -7,12 +7,25 @@ import java.awt.event.ActionListener;
 
 public class TicTacToeGui implements ActionListener {
     private final JFrame frame;
+    private final JButton[] buttons = new JButton[9];
 
     public TicTacToeGui() {
         frame = new JFrame("TicTacToe");
         frame.setLayout(new BorderLayout());
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(3, 3));
+
+        for (int i = 0; i < buttons.length; i++) {
+            buttons[i] = new JButton();
+            buttons[i].setFocusable(false);
+            buttons[i].addActionListener(this);
+            buttonPanel.add(buttons[i]);
+        }
+
+        frame.add(buttonPanel, BorderLayout.CENTER);
+        frame.pack();
         frame.setVisible(true);
     }
 
