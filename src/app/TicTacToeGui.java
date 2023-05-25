3fbe1270e3e7d636595ab6147f36fcea58ca00a4
Karol Programuje <karol341654@gmail.com>
2023-05-25 11:47:35 +0200

    Adding title panel, adding JLabel - text field

--- src/app/TicTacToeGui.java
+++ src/app/TicTacToeGui.java
@@ -8,6 +8,8 @@ import java.awt.event.ActionListener;
 public class TicTacToeGui implements ActionListener {
     private final JFrame frame;
     private final JButton[] buttons = new JButton[9];
+    private final JLabel textField;
+    private final Font font = new Font("Ink Free", Font.BOLD, 75);
 
     public TicTacToeGui() {
         frame = new JFrame("TicTacToe");
@@ -24,8 +26,23 @@ public class TicTacToeGui implements ActionListener {
             buttonPanel.add(buttons[i]);
         }
 
+        JPanel titlePanel = new JPanel();
+        titlePanel.setLayout(new BorderLayout());
+        titlePanel.setBounds(0, 0, 800, 100);
+
+        textField = new JLabel();
+        textField.setFocusable(false);
+        textField.setHorizontalAlignment(JLabel.CENTER);
+        textField.setText("Tic-Tac-Toe");
+        textField.setOpaque(true);
+        textField.setFont(font);
+
+        titlePanel.add(textField, BorderLayout.NORTH);
+
+        frame.add(titlePanel, BorderLayout.NORTH);
         frame.add(buttonPanel, BorderLayout.CENTER);
         frame.pack();
+        frame.setResizable(false);
         frame.setVisible(true);
     }
 
